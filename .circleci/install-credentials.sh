fc2e17dc0c0ec504bd449d7839562ca3531b5a32
prithvi.parit <prithvilee22@gmail.com>
2022-03-20 13:29:33 +0530

    changes for step function2

--- .circleci/install-credentials.sh
+++ .circleci/install-credentials.sh
@@ -1,4 +1,3 @@
 npx sls config credentials --provider aws --profile user2 --key "$AWS_PROFILE_KEY" --secret "$AWS_PROFILE_SECRET"
 
-
-serverless plugin install --name serverless-step-functions
+( cd step-func ; serverless plugin install --name serverless-step-functions)
